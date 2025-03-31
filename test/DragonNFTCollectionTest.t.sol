03fb1c58b5b7520ce61231ab9dd7a96d610eec30
Alberto <albertofm1992@gmail.com>
2025-03-31 22:05:35 +0300

    Added new unit tests

--- test/DragonNFTCollectionTest.t.sol
+++ test/DragonNFTCollectionTest.t.sol
@@ -38,41 +38,72 @@ contract DragonNFTCollectionTest is Test {
         vm.deal(user,userBalance);
         nftCollection.safeMint{value: mintFee}();
         assert(nftCollection.ownerOf(0) == user);
+        assert(address(nftCollection).balance == mintFee);
+       
         vm.stopPrank();
     }
 
     // Test mint with no funds
     function testNoFundsMintNFT() public {
         vm.startPrank(user);
+        vm.expectRevert("Invalid amount");
         nftCollection.safeMint();
-        assert(nftCollection.ownerOf(0) == user);
+        vm.stopPrank();
+    }
+
+    // Test mint incorrect value
+    function testIncorrectValueMintNFT() public {
+        uint256 userBalance = 1 ether;
+        uint256 invalidAmount = 0.5 ether;
+        vm.startPrank(user);
+        vm.deal(user,userBalance);
+        vm.expectRevert("Invalid amount");
+        nftCollection.safeMint{value: invalidAmount}();
         vm.stopPrank();
     }
 
     // Test mint different users
     function testMintNFTDifferentUsers() public {
+        uint256 usersBalance = 1 ether;
         for (uint256 i = 0; i < MAX_SUPPLY; i++) {
             address userMint = vm.addr(i + 1);
-            vm.prank(userMint);
-            nftCollection.safeMint();
+            vm.startPrank(userMint);
+            vm.deal(userMint,usersBalance);
+            nftCollection.safeMint{value: mintFee}();
             assert(nftCollection.ownerOf(i) == userMint);
+            vm.stopPrank();    
         }
     }
 
-    function testMintUpToMaxSupply() public {
-        vm.startPrank(user);
-        for (uint256 i = 0; i < MAX_SUPPLY; i++) {
-            nftCollection.safeMint();
-        }
+   // Test mint mint more that max supply
+    function testMintNFTMaxSupply() public {
+        uint256 usersBalance = 1 ether;
+        address userMintMax = vm.addr(1);
 
+        for (uint256 i = 0; i < MAX_SUPPLY; i++) {
+            address userMint = vm.addr(i + 2);
+            vm.startPrank(userMint);
+            vm.deal(userMint,usersBalance);
+            nftCollection.safeMint{value: mintFee}();
+            assert(nftCollection.ownerOf(i) == userMint);
+            vm.stopPrank();    
+        }
+        
+
+        vm.startPrank(userMintMax);
+        vm.deal(userMintMax,usersBalance);
         vm.expectRevert("Max Supply reached");
-        nftCollection.safeMint();
+        nftCollection.safeMint{value: mintFee}();
         vm.stopPrank();
+
+
     }
 
     function testTokenURI() public {
+        uint256 userBalance = 1 ether;
         vm.startPrank(user);
-        nftCollection.safeMint();
+        vm.deal(user,userBalance);
+        nftCollection.safeMint{value: mintFee}();
 
         string memory expectedURI = string.concat(BASE_URI, "0.json");
         assertEq(nftCollection.tokenURI(0), expectedURI);
